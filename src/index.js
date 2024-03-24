4ff105d0bcdeb05886e68e428b73afff9ec5a2c3
Abhay <robertsteeve789@gmail.com>
2024-03-24 15:12:39 +0530

    Completed upto step 8
    Signed-off-by: Abhay <robertsteeve789@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,8 +4,34 @@ const parseQuery = require('./queryParser');
 const readCSV = require('./csvReader');
 
 async function executeSELECTQuery(query) {
-    const { fields, table, whereClauses } = parseQuery(query);
-    const data = await readCSV(`${table}.csv`);
+    // const { fields, table, whereClauses } = parseQuery(query);
+    // const data = await readCSV(`${table}.csv`);
+
+    // src/index.js at executeSELECTQuery
+
+// Now we will have joinTable, joinCondition in the parsed query
+const { fields, table, whereClauses, joinTable, joinCondition } = parseQuery(query);
+data = await readCSV(`${table}.csv`);
+
+// Perform INNER JOIN if specified
+if (joinTable && joinCondition) {
+    const joinData = await readCSV(`${joinTable}.csv`);
+    data = data.flatMap(mainRow => {
+        return joinData
+            .filter(joinRow => {
+                const mainValue = mainRow[joinCondition.left.split('.')[1]];
+                const joinValue = joinRow[joinCondition.right.split('.')[1]];
+                return mainValue === joinValue;
+            })
+            .map(joinRow => {
+                return fields.reduce((acc, field) => {
+                    const [tableName, fieldName] = field.split('.');
+                    acc[field] = tableName === table ? mainRow[fieldName] : joinRow[fieldName];
+                    return acc;
+                }, {});
+            });
+    });
+}
 
     //7TH STEP
     // Apply WHERE clause filtering
